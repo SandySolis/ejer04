ab373c3ab5dbe66e9349bab5ce1f9428b6be1fcd
Sandy Solis <sandysolis0305@gmail.com>
2023-09-02 21:18:48 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese su número: ");
+int numero= int.Parse(Console.ReadLine());
+
+if (numero<=74)
+{
+    Console.WriteLine("¡Felicidades, obtiene su descuento del 15%!");
+}
+else
+{
+    Console.WriteLine("¡Felicidades, obtiene su descuento del 20%!");
+}
